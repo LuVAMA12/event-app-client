19e2f53bbd2575e048cbc144c7cc8724d2e96037
LuVAMA12 <jacqueraylucie@gmail.com>
2025-03-25 15:27:11 +0100

    suppress absolete code for front

--- src/App.jsx
+++ src/App.jsx
@@ -63,18 +63,3 @@ const [error, setError] = useState(null)
 export default App
 
 
-{/* <div className="lg:w-4/5 mx-auto flex flex-wrap" key={index}>
-      <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-        <h2 className="text-sm title-font text-gray-500 tracking-widest">{service.category}</h2>
-        <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">{service.title}</h1>
-        <div className="flex mb-4">
-          
-        </div>
-        <p className="leading-relaxed">{service.description}</p>
-        
-        <div className="flex">
-          <span className="title-font font-medium text-2xl text-gray-900">{service.price} €</span>
-          <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Réservez</button>
-        </div>
-      </div>
-    </div> */}
